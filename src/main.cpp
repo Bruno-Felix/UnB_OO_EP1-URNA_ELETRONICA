ea0da4170f0b527f8724afc58dde56d62f6efadf
Bruno-Felix <bruno-a.f@hotmail.com>
2018-09-25 15:00:59 -0300

    Ajustes de funções, interface e indentação da main.c e Finalização da Função - Informações do Candidato

--- src/main.cpp
+++ src/main.cpp
@@ -9,48 +9,116 @@
 
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "candidato.hpp"
 #include "presidente.hpp"
 
 using namespace std;
 
-int main (){
+void menu_options();
+void escolha();
+void resposta();
+void saida();
 
-	Presidente * presidente1 = new Presidente();
-	int escolha_menu, numero_de_eleitores;
+int main (){
+	//Menu
+	menu_options();
+	return 0;
+}
+
+void menu_options(){
+	system("clear");
 	//Introdução
 	cout << endl << "		      URNA ELETRONICA" << endl << endl;
 	cout << "		Eleição Geral Federal 2018" << endl << "		   1º Turno - 07/10/2018" << endl;
 	
 	//Menu
-		cout << endl << endl <<"---------------------------------------------------------" << endl;
-		cout << endl <<"MENU PRINCIPAL:" << endl;
-		cout << endl << "Digite \"1\" para Visualizar Informações dos Candidato" << endl;
-		cout << "Digite \"2\" para Iniciar uma Votação" << endl;
+	cout << endl << endl <<"---------------------------------------------------------" << endl;
+	cout <<"		      MENU PRINCIPAL:" << endl;
+	cout <<"---------------------------------------------------------" << endl;
+
+	cout << endl << "Digite \"I\" para Visualizar Informações dos Candidato" << endl;
+	cout << "Digite \"V\" para Iniciar uma Votação" << endl;
+	cout << "Digite \"S\" para Sair" << endl;
+
+	escolha();
+}
+
+void escolha(){
 	
+	Presidente * presidente1 = new Presidente();
+	int numero_de_eleitores;
+	char escolha_menu;
+
+	//Escolhas, 1 para Informaçoes dos Candidatos e 2 para Votação 2018
+	cout << endl <<"Escolha do Menu: ";
+	cin >> escolha_menu;
+
+	if (escolha_menu < 97) {
+		// Transforma as letras maiúsculas da escolha_menu do usuario em minúsculas
+		escolha_menu = escolha_menu + 32;
+	}
+
+	//Teste de exceção para a Escolha do Menu
+	while(escolha_menu != 'i' && escolha_menu != 'v' && escolha_menu != 's'){
+		cout << endl << "Por Favor, Escolha uma das Opções: \"1\" ou \"2\". ";
 		cout << endl <<"Escolha do Menu: ";
 		cin >> escolha_menu;
-	
-		//Escolhas, 1 para Informaçoes dos Candidatos e 2 para Votação 2018
-		while(escolha_menu != 1 && escolha_menu != 2){
-			cout << endl << "Erro. Por Favor, Escolha uma das Opções: \"1\" ou \"2\". ";
-			cout << endl <<"Escolha do Menu: ";
-			cin >> escolha_menu;
-		}
-		//Escolha 1, Informações dos Candidatos
-		if(escolha_menu == 1){
-			presidente1->lista_candidatos();
-		}
-		//Escolha 2, Votação 2018
-		else if(escolha_menu == 2){
-			cout << endl << endl <<"---------------------------------------------------------" << endl;
-			cout << endl <<"	VOTAÇÃO 2018:" << endl;
-		
-			cout << "	Digite Número de Eleitores: ";
-			cin >> numero_de_eleitores;
-		}
-		
+	}
 
+	//Escolha 1, Informações dos Candidatos
+	if(escolha_menu == 'i'){
+		system("clear");
+		presidente1->lista_candidatos();
+		resposta();
+	}
+
+	//Escolha 2, Votação 2018
+	else if(escolha_menu == 'v'){
+		system("clear");
+		cout << endl << endl <<"---------------------------------------------------------" << endl;
+		cout << endl <<"	VOTAÇÃO 2018:" << endl;
 	
-	return 0;
+		cout << "	Digite Número de Eleitores: ";
+		cin >> numero_de_eleitores;
+	}
+	//Escolha 3, Sair
+	else if(escolha_menu == 's'){
+		saida();
+	}
+}
+
+void resposta(){
+	char resposta;
+	cout << "\n	Digite \"M\" para voltar ao menu"<< endl;
+	cout << "	Digite \"S\" para Sair" << endl;
+	cout << endl <<"	Escolha: ";
+	
+	cin >> resposta;
+
+	if(resposta < 97) {
+		// Transforma as letras maiúsculas da resposta do usuario em minúsculas
+		resposta = resposta + 32;
+	}
+
+	while(resposta != 'm' && resposta != 's'){
+		cout << endl << "	Por Favor, Escolha uma das Opções: \"M\" ou \"S\": ";
+		cin >> resposta;
+	}
+
+	if(resposta == 'm'){
+		menu_options();
+	}
+
+	else if(resposta == 's'){
+		saida();
+	}
+  
+}
+
+void saida(){
+	system("clear");
+	cout << "\n\n		Insira make run para reiniciar o programa, Obrigado!\n\n" << endl;
+	exit(0);
 }
--- src/presidente.cpp
+++ src/presidente.cpp
@@ -16,46 +16,171 @@ void Presidente::set_nome_do_vice_candidato(string nome_do_vice_candidato){
 }
 
 void Presidente::lista_candidatos(){
-    string vetor[27][58];
+    
+    //Variáveis
+    string dfcands[1238][58];
+    string presidencia[27][58];
+    string deputado[3];
+    string senador[5];
+    string governador[4];
+    string numero_do_candidato; //Número do Candidato
+    string dep_federal = "DEPUTADO FEDERAL";
+    string dep_distrital = "DEPUTADO DISTRITAL";
+    string sen = "SENADOR";
+    string sen1 = "9";
+    string sen2 = "10";
+    string gov = "GOVERNADOR";
+    string gov1 = "VICE-GOVERNADOR";
     string aux1 = "1";
     string aux2 = "2";
-    string numero;
+    int aux3;
 
-    ifstream file("data/data_consulta_cand_2018_BR.csv");
+    //Leitura dos dois arquivos
+    ifstream file("data/data_consulta_cand_2018_BR.csv");//Arquivo BR - file
+	if(!file.is_open()) cout << "ERRO NA LEITURA DO ARQUIVO BR" << '\n';
+    ifstream ip("data/data_consulta_cand_2018_DF.csv");//Arquivo DF - ip
+	if(!ip.is_open()) cout << "ERROR NA LEITURA DO ARQUIVO DF" << '\n';
 
-	if(!file.is_open()) cout << "ERROR" << '\n';
-
+    //Inserção do número do candidato
+    aux3 = 0;
     cout << endl << endl <<"---------------------------------------------------------" << endl;
 	cout << endl <<"	INFORMAÇÕES DOS CANDIDATOS:" << endl;
-		
 	cout << endl <<"	Digite Número do Candidato para Busca: ";
-	cin >> numero;
+	cin >> numero_do_candidato;
 
+    //Leitura dos arquivos
+    //Leitura do arquivo BR
     for(int i=0; i<27; i++){
 		for(int j=0; j<58; j++){
 			if(j!=57){
-				getline(file, vetor[i][j], ';');	
-                vetor[i][j] = vetor[i][j].substr(1, vetor[i][j].size() - 2);
+				getline(file, presidencia[i][j], ';');	
+                presidencia[i][j] = presidencia[i][j].substr(1, presidencia[i][j].size() - 2);//Retirando as aspas das strings
+
 			}
 			else{
-				getline(file, vetor[i][j], '\n');
-                vetor[i][j] = vetor[i][j].substr(1, vetor[i][j].size() - 2);
+				getline(file, presidencia[i][j], '\n');
+                presidencia[i][j] = presidencia[i][j].substr(1, presidencia[i][j].size() - 2);//Retirando as aspas das strings
 			}
-        }
-        
-        if(numero == vetor[i][16] && vetor[i][13] == aux1){
-            numero_do_candidato = vetor[i][16];
-            nome_do_candidato = vetor[i][18];
-            partido = vetor[i][28];
-        }
-
-        else if(numero == vetor[i][16] && vetor[i][13] == aux2){
-            nome_do_vice_candidato = vetor[i][18];
         }
     }
 
-    cout << "\n\n\tCANDIDATO À PRESIDENCIA: " << nome_do_candidato << ";" << endl << endl;
-    cout << "\t"<<"VICE-PRESIDENCIA: " << nome_do_vice_candidato << ";" << endl;
-    cout << "\tNUMERO DO PARTIDO: " << numero_do_candidato << ";" <<endl;
-    cout << "\tNOME DO PARTIDO: " << partido << "." <<endl;
+    //Leitura do arquivo DF
+    for(int i=0; i<1238; i++){
+		for(int j=0; j<58; j++){
+			if(j!=57){
+				getline(ip, dfcands[i][j], ';');	
+                dfcands[i][j] = dfcands[i][j].substr(1, dfcands[i][j].size() - 2);
+			}
+
+			else{
+				getline(ip, dfcands[i][j], '\n');
+                dfcands[i][j] = dfcands[i][j].substr(1, dfcands[i][j].size() - 2);
+			}
+        }
+    }
+    
+    //Armazenamento dos dados:
+    //[i][28] = partido
+    //[i][16] = numero do partido
+    //[i][18] = nome
+    //[i][14] = cargo
+
+    //Armazenamento dos dados BR
+    for(int i=0; i<1238; i++){
+		for(int j=0; j<58; j++){
+            if(numero_do_candidato == presidencia[i][16] && presidencia[i][13] == aux1){
+                numero_do_candidato = presidencia[i][16];
+                nome_do_candidato = presidencia[i][18];
+                partido = presidencia[i][28];
+                aux3 = 1;
+            }
+        
+            else if(numero_do_candidato == presidencia[i][16] && presidencia[i][13] == aux2){
+                nome_do_vice_candidato = presidencia[i][18];
+            }
+        }
+    }
+
+    //Armazenamento dos dados DF
+    for(int i=0; i<1238; i++){
+		for(int j=0; j<58; j++){
+            //Deputados
+            if(numero_do_candidato == dfcands[i][16] && dfcands[i][14] == dep_federal){
+                deputado[0] = dfcands[i][18];
+                deputado[1] = dfcands[i][16];
+                deputado[2] = dfcands[i][28];
+                aux3 = 2;
+            }
+            else if(numero_do_candidato == dfcands[i][16] && dfcands[i][14] == dep_distrital){
+                deputado[0] = dfcands[i][18];
+                deputado[1] = dfcands[i][16];
+                deputado[2] = dfcands[i][28];
+                aux3 = 2;
+            }
+            //Senador
+            else if(numero_do_candidato == dfcands[i][16] && dfcands[i][14] == sen){
+                senador[0] = dfcands[i][18];
+                senador[1] = dfcands[i][16];
+                senador[2] = dfcands[i][28];
+                aux3 = 2;
+            }
+            else if(numero_do_candidato == dfcands[i][16] && dfcands[i][13] == sen1){
+                senador[3] = dfcands[i][18];
+            }
+            else if(numero_do_candidato == dfcands[i][16] && dfcands[i][13] == sen2){
+                senador[4] = dfcands[i][18];
+            }
+            //Governador
+            else if(numero_do_candidato == dfcands[i][16] && dfcands[i][14] == gov){
+                governador[0] = dfcands[i][18];
+                governador[1] = dfcands[i][16];
+                governador[2] = dfcands[i][28];
+                aux3 = 2;
+            }
+            else if(numero_do_candidato == dfcands[i][16] && dfcands[i][14] == gov1){
+                governador[3] = dfcands [i][18];
+            }
+        }
+    }
+
+    //Imprime dados BR
+    if(aux3 == 1){
+        cout << "\n\n\tCANDIDATO À PRESIDENCIA: " << nome_do_candidato << ";" << endl << endl;
+        cout << "\t"<<"VICE-PRESIDENCIA: " << nome_do_vice_candidato << ";" << endl;
+        cout << "\tNUMERO DO PARTIDO: " << numero_do_candidato << ";" <<endl;
+        cout << "\tNOME DO PARTIDO: " << partido << "." <<endl;
+    }
+
+    //Imprime dados DF
+    else if(aux3 == 2){
+        for(int i=0; i<1238; i++){
+            //Deputado Distrital
+            if(numero_do_candidato == dfcands[i][16] && dfcands[i][14] == dep_federal){
+                cout << "\n\n\tCANDIDATO: " << deputado[0] << ";" << endl << endl;
+                cout << "\tNUMERO DO PARTIDO: " << deputado[1] << ";" <<endl;
+                cout << "\tNOME DO PARTIDO: " << deputado[2] << "." <<endl<< endl;
+            }
+            //Deputado Federal
+            else if(numero_do_candidato == dfcands[i][16] && dfcands[i][14] == dep_distrital){
+                cout << "\n\n\tCANDIDATO: " << deputado[0] << ";" << endl << endl;
+                cout << "\tNUMERO DO PARTIDO: " << deputado[1] << ";" <<endl;
+                    cout << "\tNOME DO PARTIDO: " << deputado[2] << "." <<endl<< endl;
+            }
+            //Governador
+            else if(numero_do_candidato == dfcands[i][16] && dfcands[i][14] == gov){
+                cout << "\n\n\tCANDIDATO: " << governador[0] << ";" << endl << endl;
+                cout << "\tVICE-GOVERNADOR: " << governador[3] << ";" << endl;
+                cout << "\tNUMERO DO PARTIDO: " << governador[1] << ";" << endl;
+                cout << "\tNOME DO PARTIDO: " << governador[2] << "." << endl << endl;
+            }
+            //Senador
+            else if(numero_do_candidato == dfcands[i][16] && dfcands[i][14] == sen){
+                cout << "\n\n\tCANDIDATO: " << senador[0] << ";" << endl << endl;
+                cout << "\t1º SUPLENTE: " << senador[3] << ";" << endl;
+                cout << "\t2º SUPLENTE: " << senador[4] << ";" << endl;
+                cout << "\tNUMERO DO PARTIDO: " << senador[1] << ";" << endl;
+                cout << "\tNOME DO PARTIDO: " << senador[2] << "." << endl << endl;
+            }
+        }
+    }
 }
